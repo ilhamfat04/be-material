50bfcbef9f094dd6d2283ce8ca13392df076ef7d
ilham-fat <ilhamfathullah04@gmail.com>
2023-06-22 10:58:20 +0700

    complex data type

---
+++ array-of-object.js
@@ -0,0 +1,27 @@
+let cardElement = document.getElementById("card-list");
+
+let data = [
+  {
+    name: "Ilham Fathullah",
+    image: "./assets/ilham.jpg",
+    age: 23,
+  },
+  {
+    name: "Cintara Surya Elidanto",
+    image: "./assets/cintara.jpg",
+    age: 20,
+  },
+];
+
+cardElement.innerHTML = "";
+for (let i = 0; i < data.length; i++) {
+  cardElement.innerHTML += `
+        <div class="card col-sm-4" style="margin: 10px;">
+            <div class="card-body">
+                <h5 class="card-title">${data[i].name}</h5>
+                <img src="${data[i].image}" class="card-img">
+                <p class="card-text">Age : ${data[i].age}</p>
+                <a href="#" class="btn btn-primary">Like <i class="fa-regular fa-thumbs-up"></i></a>
+            </div>
+        </div>`;
+}
